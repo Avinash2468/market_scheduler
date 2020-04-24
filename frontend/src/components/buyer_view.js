d1e215247503339432e5fcc9cbe17b35b23644cc
Avinash2468 <avinashprabhu2001@gmail.com>
2020-04-24 22:49:18 +0530

    front end completed

--- frontend/src/components/buyer_view.js
+++ frontend/src/components/buyer_view.js
@@ -1,15 +1,17 @@
 import React, { Component, ReactDOM } from 'react';
 import axios from 'axios';
-//import users from './user_list_test.js';
+import '../App.css';
 export default class Buyer_view extends Component {
   constructor(props) {
        super(props);
        this.state = {lolol: []}
+       this.storeuser = this.storeuser.bind(this);
    }
 
    componentDidMount(){
        const send={
-           pincode: localStorage.getItem("pincode")
+           pincode: localStorage.getItem("pincode"),
+           ref:''
        }
        axios.post('http://localhost:4000/buyer/view',send)
            .then(response => {
@@ -22,16 +24,25 @@ export default class Buyer_view extends Component {
                console.log(error);
            })
    }
+   storeuser(data)
+   {
+     localStorage.setItem("Seller_user",data);
+   }
     render() {
         return (
           <div>
+          <h1>Available shops in your area</h1>
+          <div className="items">
           {this.state.lolol.map((usermeh, index) => (
+            <div className="itemsubs">
             <ul>
-              <a href="/buyer_view_items"><h1>{usermeh.username}</h1></a>
-              <li>{usermeh.shopname}</li>
-              <li>{usermeh.phone}</li>
+              <a href = "/buyer_view_items" key = {index} onClick={() => { this.storeuser(usermeh.username);}}><h3>ShopName:{usermeh.shopname}</h3></a>
+              <li>Name of shopkeeper:{usermeh.username}</li>
+              <li>Contact Shopkeeper:{usermeh.phone}</li>
             </ul>
+            </div>
           ))}
+          </div>
           </div>
         );
     }
